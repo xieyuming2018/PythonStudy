8adf3dc709bf7ee1d3a94853b9688d5dab58b805
xieyuming2018 <43229652+xieyuming2018@users.noreply.github.com>
2018-09-15 11:43:51 +0800

    just test Git connection.

---
+++ bubble_sort.py
@@ -0,0 +1,22 @@
+"""
+
+https://en.wikipedia.org/wiki/Bubble_sort
+
+Worst-case performance: O(N^2)
+
+"""
+
+
+def bubble_sort(arr):
+    def swap(i, j):
+        arr[i], arr[j] = arr[j], arr[i]
+
+    n = len(arr)
+    swapped = True
+    while swapped:
+        swapped = False
+        for i in range(1, n):
+            if arr[i - 1] > arr[i]:
+                swap(i - 1, i)
+                swapped = True
+    return arr
